6e22fb78ae21173114ed7770b301378136ba64ff
balogunvictor <praisetheultimate@gmail.com>
2022-12-03 21:31:27 +0100

    searched function with useMemo

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -1,27 +1,20 @@
-import "./Rightbar.scss"
-import Video from "../Video/Video"
-import {Videos} from "../../data"
+import "./Rightbar.scss";
+import Video from "../Video/Video";
+// import {Videos} from "../../data"
 // import Slide from "../Slide/Slide"
 
-
-const Rightbar = ({filterVideos}) => {
-
-  
-
-
+const Rightbar = ({ filteredVideo }) => {
   return (
     <div className="rightbar">
-    <h1>Recommended</h1>
-    {/*<Slide />*/}
-    <div className="rightbarWrapper">
-    {
-      Videos.map((v) => (
-        <Video key={v.id} video={v}/>
-      ))
-    }
+      <h1>Recommended</h1>
+      {/*<Slide />*/}
+      <div className="rightbarWrapper">
+        {filteredVideo.map((v) => (
+          <Video key={v.id} video={v} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-} 
+  );
+};
 
-export default Rightbar
+export default Rightbar;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,41 +1,40 @@
 import "./Homepage.scss";
-import Header from '../../components/Header/Header';
+import Header from "../../components/Header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Rightbar from "../../components/Rightbar/Rightbar";
 import SideIcons from "../../components/side-icons/SideIcons";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Videos } from "../../data";
 
-
 const Homepage = () => {
-  
   const [open, setOpen] = useState(false);
-  const [searchField , setSearchField] = useState('');
+  const [searchField, setSearchField] = useState("");
 
-  const onChange = e => setSearchField(e.target.value);
-  
- 
-  const handleOpen= () => {
-    setOpen((prev)=>!prev)
-  }
+  const onChange = (e) => setSearchField(e.target.value);
 
-  const filteredVideo = Videos.filter( Videos => Videos.title.toLowerCase().includes(searchField.toLowerCase())
-  );
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const filteredVideo = useMemo(() => {
+    if (searchField.length < 1) return Videos;
+    else {
+      return Videos.filter((Video) =>
+        Video.title.toLowerCase().includes(searchField.toLowerCase())
+      );
+    }
+  }, [searchField]);
 
   return (
     <>
-    <Header 
-    onClickBar={handleOpen} 
-    onChange={onChange}
-    />
-    <div className="homeContainer"> 
-
-        {open ? <SideIcons />  : <Sidebar />} 
-      
-        <Rightbar filteredVideo={filteredVideo}/>
+      <Header onClickBar={handleOpen} onChange={onChange} />
+      <div className="homeContainer">
+        {open ? <SideIcons /> : <Sidebar />}
+        
+        <Rightbar filteredVideo={filteredVideo} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
